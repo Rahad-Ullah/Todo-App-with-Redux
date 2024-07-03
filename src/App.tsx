8bc79870998f4d515e63fb5afeb41c3a222c8678
Rahad-Ullah <myemail.abc20@gmail.com>
2024-07-03 12:04:14 +0600

    Project created and Redux connected

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Todo Application</h1>
+      <Button>My Button</Button>
+    </div>
+  );
+};
+
+export default App;
