9011d37f321bec35c6bcc26c8c1d9afbc25d572f
Rahad-Ullah <myemail.abc20@gmail.com>
2024-07-04 09:18:37 +0600

    Getting data from api by RTK query

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,10 +1,14 @@
-import { useAppSelector } from "@/redux/hook";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
+import { useGetTodosQuery } from "@/redux/api/api";
 
 const TodoContainer = () => {
-  const { todos } = useAppSelector((state) => state.todos);
+  // local store
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  // remote api
+  const {data: todos}= useGetTodosQuery(undefined)
 
   return (
     <div>
@@ -14,9 +18,12 @@ const TodoContainer = () => {
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl p-2">
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
-          {todos.map((item) => (
-            <TodoCard key={item.id} {...item}/>
-          ))}
+          {todos?.length ? (
+            todos.map((item) => <TodoCard key={item.id} {...item} />)
+          ) : (
+            <div className="text-center font-semibold text-gray-500">Your Todo list is empty.
+            </div>
+          )}
         </div>
       </div>
     </div>
